c58fcc8287fb61d5e06551b69e0bb9978e3f5520
Maria Zhantal A. Villaroman <mariazhantalalvaranvillaroman@gmail.com>
2023-11-15 22:53:20 +0800

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+// const categoriesElements = document.querySelectorAll(".item");
+// console.log("Number of categories:", categoriesElements.length);
+
+// categoriesElements.forEach((el) => {
+// const categoryName = el.firstElementChild.textContent;
+// console.log("Category:", categoryName);
+
+// const categoryTypesList = el.lastElementChild;
+// const categoryTypesLength = categoryTypesList.children.length;
+// console.log("Elements:", categoryTypesLength);
+// });
+
+
+
+// const items = document.querySelectorAll(".item");
+
+// console.log("Number of Categories: ", items.length);
+
+// items.forEach(item => {
+//     const header = item.querySelector("h2").textContent;
+//     const listItem = item.querySelectorAll("li");
+
+//     console.log("Category :",header);
+//     console.log("Elements :", listItem.length);
+// });
