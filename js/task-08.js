dd36bb8163ffd2f7dbc7a13b3bee7f4e7a318719
Maria Zhantal A. Villaroman <mariazhantalalvaranvillaroman@gmail.com>
2023-11-17 07:41:14 +0800

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,3 @@
-/* Example 2 ('Submit' event on forms)*/
-
 const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', handleSubmit);
@@ -7,14 +5,14 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  // prettier-ignore
   const { elements: { email, password }} = event.currentTarget;
   console.dir(event.currentTarget);
 
   if (email.value === '' || password.value === '') {
     return console.log('Please fill in all the fields!');
   }
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  
+  console.log({ email: email.value, password: password.value });
 
   event.currentTarget.reset();
 }
